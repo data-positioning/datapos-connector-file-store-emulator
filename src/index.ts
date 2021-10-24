1c262cd26b01c3a370f941cc9051620ae3ba0bb9
Jonathan Terrell <terrell.jm@gmail.com>
2021-10-24 11:53:10 +0200

    updat

--- src/index.ts
+++ src/index.ts
@@ -22,17 +22,13 @@ import {
     extractLastSubDirectoryFromPath
 } from '../../nectis-connector-interface';
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Declarations - Variables
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 const sourceURLPrefix = 'https://nectis-sample-data.web.app/fileShare';
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Declarations - Classes
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Connector
+// -----------------------------------------------------------------------------------------------------------------------------------------
 
-class SampleFileConnector implements Connector {
+export class SampleFileConnector implements Connector {
     appEnvironment: AppEnvironment;
     appSession: AppSession;
     connection: Connection;
@@ -84,84 +80,13 @@ class SampleFileConnector implements Connector {
     }
 
     async retrieveItems(accountId: string, sessionAccessToken: string, directory: string): Promise<SourceItemPage> {
-        return itemList(directory);
+        return retrieveItems(directory);
     }
 }
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Exports
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-export default SampleFileConnector;
-
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Procedure - Item - List
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-const itemList = (directory: string): SourceItemPage => {
-    const items: SourceItem[] = [];
-    if (directory.startsWith('/SAP Employee Central')) {
-        items.push(objectItemBuilder('/SAP Employee Central', 'ADDRESS_INFO.csv', 'utf-8', 208015, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'COMP_CUR_CONV.csv', 'utf-8', 2245, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'EMP_COMP_INFO.csv', 'utf-8', 1665179, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'EMP_PAYCOMP_RECURRING.csv', 'utf-8', 1551764, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'EMPLOYMENT_INFO.csv', 'utf-8', 128575, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'EVENT_REASONS.csv', 'utf-8', 7775, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'FREQUENCY.csv', 'utf-8', 1704, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'GENERIC_OBJECTS.csv', 'utf-8', 1662477, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'GENERIC_RELATIONSHIPS.csv', 'utf-8', 98782, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'JOB_CLASS.csv', 'utf-8', 338260, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'JOB_INFO.csv', 'utf-8', 1546379, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'LABELS.csv', 'utf-8', 126838, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'LOCATIONS.csv', 'utf-8', 2995, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'PAY_COMPONENT.csv', 'utf-8', 1234, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'PERSON_INFO_GLOBAL.csv', 'utf-8', 82438, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'PERSON.csv', 'utf-8', 44896, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'PERSONAL_DATA.csv', 'utf-8', 105949, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'PICKLISTS.csv', 'utf-8', 78044, '2018-01-02T23:33:00+00:00'));
-        items.push(objectItemBuilder('/SAP Employee Central', 'TERRITORY.csv', 'utf-8', 8541, '2018-01-02T23:33:00+00:00'));
-    } else {
-        items.push(folderItemBuilder('/SAP Employee Central', 19));
-    }
-    return { cursor: undefined, isMore: false, items };
-};
-
-const folderItemBuilder = (directory: string, itemCount: number): SourceItem => {
-    const lastSubDirectoryName = extractLastSubDirectoryFromPath(directory);
-    return {
-        _id: undefined,
-        childCount: itemCount,
-        directory,
-        encodingId: undefined,
-        extension: undefined,
-        id: undefined,
-        insertedId: undefined,
-        kind: undefined,
-        label: lastSubDirectoryName,
-        lastModifiedAt: undefined,
-        name: lastSubDirectoryName,
-        referenceId: undefined,
-        size: undefined,
-        typeId: ItemTypeId.Folder
-    };
-};
-
-const objectItemBuilder = (directory: string, name: string, encodingId: string, size: number, lastModifiedAtString: string): SourceItem => ({
-    _id: undefined,
-    childCount: undefined,
-    directory,
-    encodingId,
-    extension: 'csv',
-    id: name,
-    insertedId: undefined,
-    kind: 'text/csv',
-    label: name,
-    lastModifiedAt: Date.parse(lastModifiedAtString),
-    name,
-    referenceId: undefined,
-    size,
-    typeId: ItemTypeId.Object
-});
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Preview Item
+// -----------------------------------------------------------------------------------------------------------------------------------------
 
 const previewItem = async (
     thisConnector: Connector,
@@ -185,3 +110,72 @@ const previewItem = async (
         console.log(9999, error);
     }
 };
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Retrieve Items
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+const retrieveItems = (directory: string): SourceItemPage => {
+    const items: SourceItem[] = [];
+    if (directory.startsWith('/SAP Employee Central')) {
+        items.push(buildObjectItem('/SAP Employee Central', 'ADDRESS_INFO.csv', 'utf-8', 208015, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'COMP_CUR_CONV.csv', 'utf-8', 2245, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'EMP_COMP_INFO.csv', 'utf-8', 1665179, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'EMP_PAYCOMP_RECURRING.csv', 'utf-8', 1551764, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'EMPLOYMENT_INFO.csv', 'utf-8', 128575, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'EVENT_REASONS.csv', 'utf-8', 7775, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'FREQUENCY.csv', 'utf-8', 1704, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'GENERIC_OBJECTS.csv', 'utf-8', 1662477, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'GENERIC_RELATIONSHIPS.csv', 'utf-8', 98782, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'JOB_CLASS.csv', 'utf-8', 338260, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'JOB_INFO.csv', 'utf-8', 1546379, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'LABELS.csv', 'utf-8', 126838, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'LOCATIONS.csv', 'utf-8', 2995, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'PAY_COMPONENT.csv', 'utf-8', 1234, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'PERSON_INFO_GLOBAL.csv', 'utf-8', 82438, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'PERSON.csv', 'utf-8', 44896, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'PERSONAL_DATA.csv', 'utf-8', 105949, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'PICKLISTS.csv', 'utf-8', 78044, '2018-01-02T23:33:00+00:00'));
+        items.push(buildObjectItem('/SAP Employee Central', 'TERRITORY.csv', 'utf-8', 8541, '2018-01-02T23:33:00+00:00'));
+    } else {
+        items.push(buildFolderItem('/SAP Employee Central', 19));
+    }
+    return { cursor: undefined, isMore: false, items };
+};
+
+const buildFolderItem = (directory: string, itemCount: number): SourceItem => {
+    const lastSubDirectoryName = extractLastSubDirectoryFromPath(directory);
+    return {
+        _id: undefined,
+        childCount: itemCount,
+        directory,
+        encodingId: undefined,
+        extension: undefined,
+        id: undefined,
+        insertedId: undefined,
+        kind: undefined,
+        label: lastSubDirectoryName,
+        lastModifiedAt: undefined,
+        name: lastSubDirectoryName,
+        referenceId: undefined,
+        size: undefined,
+        typeId: ItemTypeId.Folder
+    };
+};
+
+const buildObjectItem = (directory: string, name: string, encodingId: string, size: number, lastModifiedAtString: string): SourceItem => ({
+    _id: undefined,
+    childCount: undefined,
+    directory,
+    encodingId,
+    extension: 'csv',
+    id: name,
+    insertedId: undefined,
+    kind: 'text/csv',
+    label: name,
+    lastModifiedAt: Date.parse(lastModifiedAtString),
+    name,
+    referenceId: undefined,
+    size,
+    typeId: ItemTypeId.Object
+});
