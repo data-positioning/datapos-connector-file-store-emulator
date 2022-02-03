9f230536cddfe76f53442dbf964d64e84e02c619
Jonathan Terrell <terrell.jm@gmail.com>
2022-02-03 14:40:36 +0100

    Release v0.1.28

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,12 @@
  * @license "ISC"
  */
 
+// import { initializeApp } from 'firebase/app';
+// import { doc, getFirestore, setDoc } from 'firebase/firestore';
+
+const env = require('./.env.json');
+const FireStoreParser = require('firestore-parser');
+
 module.exports = (grunt) => {
     // Initialise configuration.
     grunt.initConfig({
@@ -31,12 +37,55 @@ module.exports = (grunt) => {
         }
     });
 
+    function updateFirestore() {
+        const firebaseApp = initializeApp({
+            apiKey: '### FIREBASE API KEY ###',
+            authDomain: '### FIREBASE AUTH DOMAIN ###',
+            projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+        });
+        const db = getFirestore();
+        setDoc(doc(db, 'cities', 'LA'), {
+            name: 'Los Angeles',
+            state: 'CA',
+            country: 'USA'
+        });
+    }
+
+    grunt.task.registerTask('updateFirestore', 'Updates Firestore', async function () {
+        const done = this.async();
+
+        const fetchModule = await import('node-fetch');
+        console.log(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${env.FIREBASE_API_KEY}`);
+        const response = await fetchModule.default(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${env.FIREBASE_API_KEY}`, {
+            body: JSON.stringify({
+                email: env.FIREBASE_EMAIL_ADDRESS,
+                password: env.FIREBASE_PASSWORD,
+                returnSecureToken: true
+            }),
+            headers: { Referer: 'nectis-app-v00-dev-alpha.web.app' },
+            method: 'POST'
+        });
+        const result = await response.json();
+        console.log(result.idToken);
+        const response2 = await fetchModule.default(`https://firestore.googleapis.com/v1beta1/projects/nectis-app-v00-dev-alpha/databases/(default)/documents/accounts`, {
+            headers: { Authorization: `Bearer ${result.idToken}` },
+            method: 'GET'
+        });
+
+        const aaaa = await response2.json();
+        console.log(aaaa);
+        const bbbb = await FireStoreParser(aaaa);
+        console.log(bbbb);
+
+        done();
+    });
+
     // Load external tasks.
     grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-run');
 
     // Register local tasks.
-    grunt.registerTask('audit', ['run:audit']); // CMD SHIFT A
+    grunt.registerTask('audit', ['updateFirestore']); // CMD SHIFT A
     grunt.registerTask('build', ['run:rollup_cjs', 'run:rollup_es', 'run:copyToFirebase']); // CMD SHIFT B
     grunt.registerTask('checkLicense', ['run:licenseChecker', 'run:nlf']); // CMD SHIFT C
     grunt.registerTask('lint', ['run:lint']); // CMD SHIFT L
