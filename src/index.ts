9c53eaf1f5679fd6a02cfe8cb2923486264edf87
Jonathan Terrell <terrell.jm@gmail.com>
2022-03-13 18:06:40 +0100

    Release v0.1.75

--- src/index.ts
+++ src/index.ts
@@ -92,7 +92,6 @@ const previewItem = async (
     const headers: HeadersInit = {
         Range: `bytes=0-${previewInterfaceSettings.chunkSize || defaultChunkSize}`
     };
-    console.log(`${urlPrefix}${encodeURIComponent(sourceViewProperties.path)}?alt=media`);
     const response = await fetch(`${urlPrefix}${encodeURIComponent(sourceViewProperties.path)}?alt=media`, { headers });
     if (!response.ok) throw new Error(response.statusText);
     const arrayBuffer = await response.arrayBuffer();
