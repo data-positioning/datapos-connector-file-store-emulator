76af479a0d90a50643a82ea7a4f0f2585f1b3f4e
Jonathan Terrell <terrell.jm@gmail.com>
2022-05-03 22:01:30 +0200

    Release v0.1.150

--- rollup.config-cjs.js
+++ rollup.config-cjs.js
@@ -12,7 +12,7 @@ import { terser } from 'rollup-plugin-terser';
 
 export default [
     {
-        external: [],
+        external: ['chardet', 'nanoid'],
         input: pkg.main,
         output: [
             {
