16a2c27db925f5c3b92ac2d7fb8e5f5f9e2251bc
Jonathan Terrell <terrell.jm@gmail.com>
2022-06-12 21:11:48 +0200

    new files

---
+++ rollup.config-iife.js
@@ -0,0 +1,29 @@
+/**
+ * @author Jonathan Terrell <terrell.jm@gmail.com>
+ * @copyright 2022 Jonathan Terrell
+ * @file dataposapp-connector-data-file-store-emulator/rollup.config-umd.js
+ * @license ISC
+ */
+
+import camelcase from 'lodash.camelcase';
+import config from './src/config.json';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+    {
+        external: ['chardet', 'csv-parse/browser/esm'], // TODO: Can be removed when 'engine main' is published as an npm package.
+        input: pkg.main,
+        output: [
+            {
+                exports: 'auto',
+                file: `./dist/${config.id}-iife.js`,
+                format: 'iife',
+                name: camelcase(config.id)
+            }
+        ],
+        plugins: [json(), typescript(), terser({ output: { comments: false } })]
+    }
+];
