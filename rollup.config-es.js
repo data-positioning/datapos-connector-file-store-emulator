1e346b4ecfaf71555ee62113726162c01543a489
Jonathan Terrell <terrell.jm@gmail.com>
2022-10-20 16:14:10 +0200

    Release v0.1.352

--- rollup.config-es.js
+++ rollup.config-es.js
@@ -24,6 +24,6 @@ export default [
                 format: 'es'
             }
         ],
-        plugins: [nodeResolve({ browser: true }), commonjs(), json(), typescript()]
+        plugins: [nodeResolve({ browser: true }), commonjs(), json(), typescript(), terser({ output: { comments: false } })]
     }
 ];
