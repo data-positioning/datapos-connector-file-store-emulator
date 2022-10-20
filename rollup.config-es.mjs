5815c1c417f4e797905adba920b8e0272f8feed4
Jonathan Terrell <terrell.jm@gmail.com>
2022-10-20 21:05:17 +0200

    new files

---
+++ rollup.config-es.mjs
@@ -0,0 +1,28 @@
+/**
+ * @author Jonathan Terrell <terrell.jm@gmail.com>
+ * @copyright 2022 Jonathan Terrell
+ * @file dataposapp-connector-data-file-store-emulator/rollup.config-es.js
+ * @license ISC
+ */
+
+import commonjs from '@rollup/plugin-commonjs';
+import config from './src/config.json' assert { type: 'json' };
+import nodeResolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+    {
+        external: [],
+        input: 'src/index.ts',
+        output: [
+            {
+                exports: 'auto',
+                file: `./dist/${config.id}-es.js`,
+                format: 'es'
+            }
+        ],
+        plugins: [nodeResolve({ browser: true }), commonjs(), json(), typescript(), terser({ output: { comments: false } })]
+    }
+];
