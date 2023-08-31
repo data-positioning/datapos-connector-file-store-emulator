c8668c3392f68aec6eedd3c569007755455906c6
Jonathan Terrell <terrell.jm@gmail.com>
2023-08-31 23:40:12 +0200

    v0.1.773

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-// Dependencies - Framework/Vendor
 import config from './src/config.json';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
-// Configuration.
 export default defineConfig({
     build: {
-        target: 'ESNext',
         lib: {
             entry: resolve('src/index.ts'),
-            name: 'DataPosEngine',
+            name: 'DataposFileStoreEmulatorDataConnector',
             formats: ['es'],
             fileName: (format) => `${config.id}-${format}.js`
-        }
+        },
+        target: 'ESNext'
     },
-    plugins: [dts({ outputDir: 'dist/types' })]
+    plugins: [dts({ outDir: 'dist/types' })]
 });
