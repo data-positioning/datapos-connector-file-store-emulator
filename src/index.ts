71a31c2e912f6cbffbb7a9ba19291234a27964e5
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-04-29 12:39:41 +0200

    v0.2.107

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,14 @@ import { version } from '../package.json';
 type FileStoreIndex = Record<string, { id?: string; childCount?: number; lastModifiedAt?: number; name: string; size?: number; typeId: string }[]>;
 
 // Constants
-const CALLBACK_PREVIEW_ABORTED = 'Connector preview item aborted.';
-const CALLBACK_RETRIEVE_ABORTED = 'Connector retrieve items aborted.';
+const CALLBACK_PREVIEW_ABORTED = 'Connector failed to abort preview item.';
+const CALLBACK_RETRIEVE_ABORTED = 'Connector failed to abort list items.';
 const DEFAULT_PREVIEW_CHUNK_SIZE = 4096;
 const DEFAULT_RETRIEVE_CHUNK_SIZE = 1000;
-const ERROR_FIND_ITEM_FAILED = 'Connector find item failed.';
-const ERROR_LIST_ITEMS_FAILED = 'Connector list items failed.';
-const ERROR_PREVIEW_FAILED = 'Connector preview item failed.';
-const ERROR_RETRIEVE_FAILED = 'Connector retrieve items failed.';
+const ERROR_FIND_ITEM_FAILED = 'Connector failed to find item.';
+const ERROR_LIST_ITEMS_FAILED = 'Connector failed to list items.';
+const ERROR_PREVIEW_FAILED = 'Connector failed to preview item.';
+const ERROR_RETRIEVE_FAILED = 'Connector failed to retrieve items.';
 const URL_PREFIX = 'https://sampledata.datapos.app';
 
 // Classes - File Store Emulator Connector
