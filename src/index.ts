1c2d28db8864a4bc7cf339c7ecfcf3d26e8c2e8a
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-05 23:29:54 +0100

    v0.2.218

--- src/index.ts
+++ src/index.ts
@@ -140,7 +140,7 @@ export default class FileStoreEmulatorConnector implements Connector {
                         let data;
                         while ((data = parser.read() as string[]) !== null) {
                             signal.throwIfAborted(); // Check if the abort signal has been triggered.
-                            pendingRows.push(data); // Append the row of parsed values and associated information to the pending rows array.
+                            // pendingRows.push(data); // Append the row of parsed values and associated information to the pending rows array.
                             if (pendingRows.length < DEFAULT_RETRIEVE_CHUNK_SIZE) continue; // Continue with next iteration if the pending rows array is not yet full.
                             chunk([]); // Pass the pending rows to the engine using the 'chunk' callback.
                             pendingRows = []; // Clear the pending rows array in preparation for the next batch of data.
