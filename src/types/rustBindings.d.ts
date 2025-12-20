55b89506c982db1be2b59ae22c9a5949c085e021
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-20 17:28:43 +0100

    v0.2.302

--- src/types/rustBindings.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '../rust/datapos-connector-file-store-emulator-core/pkg/datapos_connector_file_store_emulator_core.js' {
-    export function add_my_numbers(left: number, right: number): number;
-    export function checksum_from_rust(input: string): number;
-}
