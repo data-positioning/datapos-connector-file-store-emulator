3e1eb87b7c9b033408db6434e97586492f45e5cf
Jonathan Terrell <terrell.jm@gmail.com>
2026-06-23 12:53:54 +0200

    v0.2.557

---
+++ rust/dpuse-connector-file-store-emulator-core/pkg/dpuse_connector_file_store_emulator_core.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Adds two signed 32-bit integers using native Rust arithmetic.
+ */
+export function add_my_numbers(left: number, right: number): number;
+
+/**
+ * Computes a simple checksum by summing every byte in the input string.
+ */
+export function checksum_from_rust(input: string): number;
---
+++ rust/dpuse-connector-file-store-emulator-core/pkg/dpuse_connector_file_store_emulator_core.js
@@ -0,0 +1,5 @@
+import * as wasm from "./dpuse_connector_file_store_emulator_core_bg.wasm";
+export * from "./dpuse_connector_file_store_emulator_core_bg.js";
+import { __wbg_set_wasm } from "./dpuse_connector_file_store_emulator_core_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
